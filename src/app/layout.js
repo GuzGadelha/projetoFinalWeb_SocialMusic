25d6a8692d3295af9eca1730a9653e0c0ef0b579
Gustavo Gadelha <Guztv04@gmail.com>
2025-12-02 16:09:33 -0300

    commit final de guz

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import './globals.css';
+import './globals.css'; 
 import Sidebar from '@/components/Sidebar';
 
 export const metadata = { title: 'Social Music', description: 'Rede social de música' };
